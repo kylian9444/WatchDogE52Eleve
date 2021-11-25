3c8b8faad16d4ac4b70dee1deb54da50341e0784
Laurent Giustignano <laurent.giustignano@free.fr>
2021-11-25 14:43:54 +0100

    E52

---
+++ config.js
@@ -0,0 +1,19 @@
+const broker = "http://172.16.12.47";
+
+const topic1 = "turing/thermo/mesure";
+const topic2 = "turing/thermo/moyenne";
+const topic3 = "turing/porte/etat";
+const topic4 = "torvalds/thermo/mesure";
+const topic5 = "torvalds/thermo/moyenne";
+const topic6 = "torvalds/porte/etat";
+
+
+module.exports.laConfig = {
+    broker,
+    topic1,
+    topic2,
+    topic3,
+    topic4,
+    topic5,
+    topic6
+}
---
+++ js/client.js
@@ -0,0 +1,98 @@
+
+var socket = io();
+
+socket.on('turing/thermo/mesure', function(msg) {
+    var elemValeur = document.getElementById('alphavalue');
+    var elemBar = document.getElementById('alphabar');
+
+    if(msg > 25){
+        elemValeur.setAttribute("class","text-danger");
+        elemBar.setAttribute("class","progress-bar bg-danger")
+    }
+    else{
+        elemValeur.setAttribute("class","text-success");
+        elemBar.setAttribute("class","progress-bar bg-success")
+    }
+
+    elemValeur.textContent = msg;
+    elemBar.setAttribute("style","width: "+msg+"%;");
+});
+
+socket.on('turing/thermo/moyenne', function(msg) {
+    var elemValeur = document.getElementById('betavalue');
+    var elemBar = document.getElementById('betabar');
+
+    if(msg > 25){
+        elemValeur.setAttribute("class","text-danger");
+        elemBar.setAttribute("class","progress-bar bg-danger")
+    }
+    else{
+        elemValeur.setAttribute("class","text-success");
+        elemBar.setAttribute("class","progress-bar bg-success")
+    }
+    elemValeur.textContent = msg;
+    elemBar.setAttribute("style","width: "+msg+"%;");
+});
+
+socket.on('turing/porte/etat', function(msg) {
+    var elemValeur = document.getElementById('gammavalue');
+
+    if(msg === 0){
+
+        elemValeur.textContent = "Fermée";
+        elemValeur.setAttribute("class","btn btn-success disabled");
+    }
+    else{
+        elemValeur.textContent = "ouverte";
+        elemValeur.setAttribute("class","btn btn-danger disabled");
+    }
+
+});
+
+
+socket.on('torvalds/thermo/mesure', function(msg) {
+    var elemValeur = document.getElementById('deltavalue');
+    var elemBar = document.getElementById('deltabar');
+
+    if(msg > 25){
+        elemValeur.setAttribute("class","text-danger");
+        elemBar.setAttribute("class","progress-bar bg-danger")
+    }
+    else{
+        elemValeur.setAttribute("class","text-success");
+        elemBar.setAttribute("class","progress-bar bg-success")
+    }
+    elemValeur.textContent = msg;
+    elemBar.setAttribute("style","width: "+msg+"%;");
+});
+
+socket.on('torvalds/thermo/moyenne', function(msg) {
+    var elemValeur = document.getElementById('epsilonvalue');
+    var elemBar = document.getElementById('epsilonbar');
+
+    if(msg > 25){
+        elemValeur.setAttribute("class","text-danger");
+        elemBar.setAttribute("class","progress-bar bg-danger")
+    }
+    else{
+        elemValeur.setAttribute("class","text-success");
+        elemBar.setAttribute("class","progress-bar bg-success")
+    }
+    elemValeur.textContent = msg;
+    elemBar.setAttribute("style","width: "+msg+"%;");
+});
+
+socket.on('torvalds/porte/etat', function(msg) {
+    var elemValeur = document.getElementById('zetavalue');
+
+    if(msg == 0){
+
+        elemValeur.textContent = "Fermée";
+        elemValeur.setAttribute("class","btn btn-success disabled");
+    }
+    else{
+        elemValeur.textContent = "ouverte";
+        elemValeur.setAttribute("class","btn btn-danger disabled");
+    }
+});
+
